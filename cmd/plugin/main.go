8f1fb09a5763e065ee6650c3000d612715d15ac2
mwaldheim <31657644+mwaldheim@users.noreply.github.com>
2026-05-25 20:13:34 +0200

    feat: migrate implementation from semrel core
    Signed-off-by: mwaldheim <31657644+mwaldheim@users.noreply.github.com>

--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -1,25 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2026 The plugin-template Authors
+// SPDX-FileCopyrightText: 2026 The semrel Authors
 
 package main
 
 import (
-	"context"
 	"log"
-	"os"
 
-	grpcserver "github.com/SemRels/updater-go/internal/grpc"
-	semrelplugin "github.com/SemRels/updater-go/internal/plugin"
+	plugin "github.com/SemRels/updater-go/internal/plugin"
 )
 
 func main() {
-	provider := semrelplugin.NewProvider("updater-go")
-	server := grpcserver.NewProviderServer(provider)
-
-	if _, err := server.Health(context.Background()); err != nil {
-		log.Printf("plugin health check failed: %v", err)
-		os.Exit(1)
-	}
-
-	log.Printf("%s plugin template is ready", provider.Name())
+	builder := plugin.NewBuilder(plugin.BuildConfig{})
+	log.Printf("updater-go plugin ready: builds Go release archives (%T)", builder)
 }
